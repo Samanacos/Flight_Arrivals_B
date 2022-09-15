292ad944738388a44fdc9403205499683bf530b0
Giannis Terzakis <59915445+Samanacos@users.noreply.github.com>
2022-09-15 20:27:14 +0300

    Add files via upload

---
+++ FlightArrivals B/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace FlightArrivals_B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstArrivals = new System.Windows.Forms.ListBox();
+            this.lblFlight = new System.Windows.Forms.Label();
+            this.lblDeparture = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblArrivals = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstArrivals
+            // 
+            this.lstArrivals.Font = new System.Drawing.Font("Courier New", 9.75F);
+            this.lstArrivals.FormattingEnabled = true;
+            this.lstArrivals.ItemHeight = 16;
+            this.lstArrivals.Location = new System.Drawing.Point(40, 80);
+            this.lstArrivals.Name = "lstArrivals";
+            this.lstArrivals.Size = new System.Drawing.Size(300, 148);
+            this.lstArrivals.TabIndex = 17;
+            this.lstArrivals.SelectedIndexChanged += new System.EventHandler(this.lstArrivals_SelectedIndexChanged);
+            // 
+            // lblFlight
+            // 
+            this.lblFlight.AutoSize = true;
+            this.lblFlight.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFlight.ForeColor = System.Drawing.Color.Coral;
+            this.lblFlight.Location = new System.Drawing.Point(40, 60);
+            this.lblFlight.Name = "lblFlight";
+            this.lblFlight.Size = new System.Drawing.Size(46, 16);
+            this.lblFlight.TabIndex = 19;
+            this.lblFlight.Text = "Flight";
+            // 
+            // lblDeparture
+            // 
+            this.lblDeparture.AutoSize = true;
+            this.lblDeparture.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDeparture.ForeColor = System.Drawing.Color.Coral;
+            this.lblDeparture.Location = new System.Drawing.Point(160, 60);
+            this.lblDeparture.Name = "lblDeparture";
+            this.lblDeparture.Size = new System.Drawing.Size(77, 16);
+            this.lblDeparture.TabIndex = 20;
+            this.lblDeparture.Text = "Departure";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.Coral;
+            this.lblTime.Location = new System.Drawing.Point(280, 60);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(43, 16);
+            this.lblTime.TabIndex = 21;
+            this.lblTime.Text = "Time";
+            // 
+            // lblArrivals
+            // 
+            this.lblArrivals.AutoSize = true;
+            this.lblArrivals.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.lblArrivals.Location = new System.Drawing.Point(120, 10);
+            this.lblArrivals.Name = "lblArrivals";
+            this.lblArrivals.Size = new System.Drawing.Size(147, 25);
+            this.lblArrivals.TabIndex = 22;
+            this.lblArrivals.Text = "Arrivals Board";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.lblArrivals);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblDeparture);
+            this.Controls.Add(this.lblFlight);
+            this.Controls.Add(this.lstArrivals);
+            this.Name = "Form1";
+            this.Text = "Flight Arrivals";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstArrivals;
+        private System.Windows.Forms.Label lblFlight;
+        private System.Windows.Forms.Label lblDeparture;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblArrivals;
+    }
+}
+
---
+++ FlightArrivals B/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlightArrivals_B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Arrays to hold the flight information
+        string[] flightNumbers = { "KO421", "ML261", "BA376", "MTH419", "ALK238", "KLM265", "THO132", "ALM145", "THO573" };
+        string[] departAirport = { "Athens", "Manchester", "Dublin", "Salzburg", "Dallas", "Glasgow", "Geneva", "London", "Bordeaux" };
+        string[] arrivalTimes = { "14:32", "13:25", "13:16", "14:54", "14:18", "14:39", "13:57", "14:40", "15:10" };
+
+        //Declare the List
+        List<Flight> arrivalBoard = new List<Flight>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Create arrivals and add them to the List
+            for (int i = 0; i < flightNumbers.Length; i++)
+            {
+                Flight newFlight = new Flight(flightNumbers[i], departAirport[i], arrivalTimes[i]);
+                arrivalBoard.Add(newFlight);
+            }
+
+            //sort the List
+            arrivalBoard.Sort();
+
+            //Display current arrivals
+            DisplayArrivals(arrivalBoard);
+        }
+
+        //Display the List in the list box
+        void DisplayArrivals(List<Flight> flight)
+        {
+            lstArrivals.Items.Clear();
+
+            //for each object in the List
+            for (int i = 0; i < flight.Count; i++)
+            {
+                //get a Flight object and add it to the list box
+                Flight aFlight = (Flight)flight[i];
+
+                lstArrivals.Items.Add(aFlight);
+            }
+        }
+
+        private void lstArrivals_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
